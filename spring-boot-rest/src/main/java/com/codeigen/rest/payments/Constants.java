a39973c4406b5fdab2d28b6677823b25f3a284d7
gaurav-sr <sgauravs@gmail.com>
2024-02-19 22:01:35 +0000

    spring boot code

--- spring-boot-rest/src/main/java/com/codeigen/rest/payments/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.codeigen.rest.payments;
-
-public class Constants {
-    public static int CARD_ID_LENGTH = 16;
-    public static String SUCCESS_MESSAGE = "SUCCESS";
-    public static String FAIL_MESSAGE = "FAIL";
-}
--- spring-boot-rest/src/main/java/com/codeigen/rest/payments/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.codeigen.rest.payments;
-
-public interface PaymentService {
-
-    PaymentResponse processPayment(PaymentRequest paymentRequest);
-
-    PaymentResponse processPayment();
-
-}
